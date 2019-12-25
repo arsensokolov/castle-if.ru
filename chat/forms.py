969f3594efb5954e2db5c07d57ac3b66fb9723af
Arseny Sokolov <me@arsen.pw>
2019-12-26 00:08:16 +0800

    Добавлены приватные сообщения. Можно отправлять в любой комнате.

--- chat/forms.py
+++ chat/forms.py
@@ -1,10 +1,15 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from djantimat.helpers import PymorphyProc
 from .models import Chat
 
+User = get_user_model()
+
+ERROR_FLOOD_MESSAGE = 'вы уже отправляли такое сообщение'
+
 
 class ChatForm(forms.ModelForm):
-    message = forms.CharField(min_length=1, max_length=400, widget=forms.TextInput(attrs={'size': 37}))
+    message = forms.CharField(max_length=400, widget=forms.TextInput(attrs={'size': 37}))
 
     class Meta:
         model = Chat
@@ -20,5 +25,31 @@ class ChatForm(forms.ModelForm):
         last_messages = Chat.objects.filter(user=self.user).values_list('message', flat=True)[:5]
 
         if message in last_messages:
-            raise forms.ValidationError('вы уже отправляли такое сообщение', code='invalid')
+            raise forms.ValidationError(ERROR_FLOOD_MESSAGE, code='invalid')
+        return message
+
+
+class PrivateForm(ChatForm):
+    message = forms.CharField(max_length=300, widget=forms.TextInput(attrs={'size': 15}))
+
+    class Meta:
+        model = Chat
+        fields = ['to', 'message']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user = kwargs.pop('user')
+        self.fields['to'].queryset = User.objects.exclude(id=self.user.id)
+
+    def clean_message(self):
+        message = self.cleaned_data['message']
+        to_user = self.cleaned_data['to']
+        message = PymorphyProc.replace(message, repl='@@@')
+        last_messages = Chat.objects.filter(
+            user=self.user,
+            to=to_user
+        ).values_list('message', flat=True)[:5]
+
+        if message in last_messages:
+            raise forms.ValidationError(ERROR_FLOOD_MESSAGE, code='invalid')
         return message
--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,9 @@
 from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
+from django.urls import reverse
 from .models import Chat
-from .forms import ChatForm
+from .forms import ChatForm, PrivateForm
 
 
 class ChatView(LoginRequiredMixin, generic.TemplateView):
@@ -34,9 +35,10 @@ class ChatList(LoginRequiredMixin, generic.ListView):
 
 class AddMessage(LoginRequiredMixin, generic.CreateView):
     model = Chat
-    # fields = ['message']
-    success_url = '/ctrl.html'
     form_class = ChatForm
+
+    def get_success_url(self):
+        return reverse('ctrl')
 
     def get_form_kwargs(self):
         kwargs = super(AddMessage, self).get_form_kwargs()
@@ -49,3 +51,12 @@ class AddMessage(LoginRequiredMixin, generic.CreateView):
         form.instance.user = self.request.user
         form.instance.room = self.request.room
         return super(AddMessage, self).form_valid(form)
+
+
+class PrivateFrame(AddMessage, generic.CreateView):
+    model = Chat
+    form_class = PrivateForm
+    template_name = 'chat/private.html'
+
+    def get_success_url(self):
+        return reverse('private')
