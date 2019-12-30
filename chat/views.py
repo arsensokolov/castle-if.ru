263fccebabbad7733e681a4881f5f8e317badfe9
Arseny Sokolov <me@arsen.pw>
2019-12-30 20:06:07 +0800

    Добавлены правила и справка.

--- chat/views.py
+++ chat/views.py
@@ -2,7 +2,7 @@ from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 from django.urls import reverse
-from .models import Chat
+from .models import Chat, Smile
 from .forms import ChatForm, PrivateForm
 
 
@@ -20,6 +20,23 @@ class TimeFrame(LoginRequiredMixin, generic.TemplateView):
 
 class MenuFrame(LoginRequiredMixin, generic.TemplateView):
     template_name = 'chat/menu.html'
+
+
+class PravilaView(generic.TemplateView):
+    template_name = 'chat/pravila.html'
+    extra_context = {'title': 'Правила поведения в чате'}
+
+
+class HelpPages(generic.TemplateView):
+    template_name = 'help/index.html'
+    extra_context = {'title': 'Помощь заключенным'}
+
+    def get_template_names(self):
+        if self.kwargs.get('page'):
+            self.template_name = 'help/{}.html'.format(self.kwargs['page'])
+            if self.kwargs['page'] == 'smile':
+                self.extra_context.update({'smiles': Smile.objects.all()})
+        return super(HelpPages, self).get_template_names()
 
 
 class ChatList(LoginRequiredMixin, generic.ListView):
@@ -57,6 +74,7 @@ class PrivateFrame(AddMessage, generic.CreateView):
     model = Chat
     form_class = PrivateForm
     template_name = 'chat/private.html'
+    extra_context = {'title': 'Приватное сообщение'}
 
     def get_success_url(self):
         return reverse('private')
